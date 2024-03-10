d251603e8329a7f866332ca24fee56f609ddd8da
jeftounz <srjesusc@gmail.com>
2024-03-10 01:50:35 -0400

    Mi primera vista y modelo simple

--- holaMundo/home/models.py
+++ holaMundo/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#En cada modulo que se cree en django , por ejemplo el modulo "home" vas a necesitar ampliar la base de datos
+#Las bases de datos van a ser ejecutadas aqui por medio de clases 
+
+#Siempre se crea un modelo con models.Model
+class Task(models.Model):
+    description=models.CharField(max_length=255)
+    done=models.BooleanField()
--- holaMundo/home/views.py
+++ holaMundo/home/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homeView(request):
-    return HttpResponse("Hola Mundo, esta es mi primera vista!!!")
-    
+    #return HttpResponse("Hola Mundo, esta es mi primera vista!!!")
+    #Creamos un contexto , un diccionario donde se almacenen valores para renderizar
+    context={
+        'message':'Hola Mundo, esto esta en el contexto',
+        'message2':'Tareas por hacer',
+        'tasks':["Mapa","Video","Resumen","Dibujo"],
+        'emptyList':[]
+    }
+
+    return render(request,'home/index.html',context)
