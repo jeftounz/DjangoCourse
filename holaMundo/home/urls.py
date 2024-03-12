3476562cdad059fd28156e4a77ad0802e0298e28
jeftounz <srjesusc@gmail.com>
2024-03-12 00:07:36 -0400

    Mi primer formulario

--- holaMundo/home/urls.py
+++ holaMundo/home/urls.py
@@ -1,8 +1,10 @@
 #Aqui agregamos las url para ver las vistas que hay en el modelo
-from django.urls import path #Aqui importamos las path del proyecto original
-from .views import homeView #Aqui importamos la vista de urls de esta carpeta oh modulo(home)
+from django.urls import path, reverse #Aqui importamos las path del proyecto original
+from .views import homeView, sum_view, create_task #Aqui importamos la vista de urls de esta carpeta oh modulo(home)
 urlpatterns=[
-    path('',homeView,name='home') 
+    path('',homeView,name='home'),
+    path('suma/<int:num1>/<int:num2>',sum_view,name='sum'),
+    path('create_task/',create_task,name='create-task') 
 ]
 
 #NOTA: todas las urls como el de arriba ("path('',homeView,name='home') ", 
--- holaMundo/home/views.py
+++ holaMundo/home/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url
 from .models import Task #Importamos el modelo a las vistas
 def homeView(request):
     #return HttpResponse("Hola Mundo, esta es mi primera vista!!!")
@@ -14,3 +14,24 @@ def homeView(request):
     #print(Task.objects.all())
 
     return render(request,'home/index.html',context)
+
+def sum_view(request,num1,num2):
+    sum=num1+num2
+    context={
+        'sum':sum
+    }
+
+    return render(request,'home/sum.html',context)
+
+#Vista del template create_task.html 
+def create_task(request):
+
+    if request.POST:
+        new_task=request.POST.get('tarea')
+       # print("*******")
+       # print(new_task)
+       #print("*******")
+        Task.objects.create(decription=new_task)
+        return redirect(reversed('create-task'))
+
+    return render(request,'home/create_task.html')
